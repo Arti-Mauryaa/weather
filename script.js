134c7e9a34b674bfb8c9969e3cee72f84bdc43bf
aditya-ai00 <adityanyo39@gmail.com>
2026-04-18 11:53:40 +0530

    fix weather api

--- script.js
+++ script.js
@@ -2,39 +2,36 @@ const apiKey = "3a4dc2581af7beaa310a4dbc43bd8d2c";
 
 function getWeather() {
 
-const city = document.getElementById("city").value.trim();
+  const city = document.getElementById("city").value.trim();
 
-if (city === "") {
-alert("Please enter a city name");
-return;
-}
+  if (!city) {
+    alert("Please enter a city name");
+    return;
+  }
 
-const url =
-`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},IN&appid=${apiKey}&units=metric`;
 
-fetch(url)
-.then(response => response.json())
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("City not found");
+      }
+      return response.json();
+    })
+    .then(data => {
 
-.then(data => {
+      document.getElementById("name").innerText =
+        data.name + ", " + data.sys.country;
 
-if (data.cod !== 200) {
-alert("City not found");
-return;
-}
+      document.getElementById("temp").innerText =
+        Math.round(data.main.temp) + " °C";
 
-document.getElementById("name").innerText =
-data.name + ", " + data.sys.country;
+      document.getElementById("condition").innerText =
+        data.weather[0].description;
 
-document.getElementById("temp").innerText =
-Math.round(data.main.temp) + " °C";
-
-document.getElementById("condition").innerText =
-data.weather[0].description;
-
-})
-
-.catch(() => {
-alert("Error fetching weather data");
-});
+    })
+    .catch(() => {
+      alert("City not found");
+    });
 
 }
